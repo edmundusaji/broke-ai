8c1d801a8ca72d9a95872e83b8acbac9c0dc4fd7
Edmundus <edmundus.prasetyaji@gdn-commerce.com>
2026-04-04 13:09:40 +0700

    Remove unused import

--- src/main/java/org/edmund/brokeai/service/serviceimpl/GeminiOutboundServiceImpl.java
+++ src/main/java/org/edmund/brokeai/service/serviceimpl/GeminiOutboundServiceImpl.java
@@ -7,8 +7,6 @@ import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Service;
 import org.springframework.web.client.RestTemplate;
 
-import java.util.List;
-
 @Service
 public class GeminiOutboundServiceImpl implements GeminiOutboundService {
 
